c2d6751787fa6e2e4de87389939db9592575592b
Kenedy <kenedydacosta@gmail.com>
2023-12-08 15:04:16 -0300

    como funcionam os comentarios

---
+++ comentarios.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    /*Isso é um comentário, 
+    Importante: comentários NÃO SÃO válidos dentro de strings*/
+    printf("voce NAO esta vendo os comentarios");
+}

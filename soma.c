496ff00f9df2fd7e1b79b66a5f8442962438456b
Kenedy <kenedydacosta@gmail.com>
2023-12-12 16:39:14 -0300

    soma melhoradaa

--- soma.c
+++ soma.c
@@ -3,6 +3,6 @@ int main(){
     int num1, num2;
     printf("Introduza 02 numeros: ");
     scanf("%d%d",&num1,&num2);
-    printf("o resultado da soma dos numeros e: %d",num1+num2);
+    printf("o resultado da soma dos numeros %d + %d e: %d",num1,num2,num1+num2);
     return 0;
 }

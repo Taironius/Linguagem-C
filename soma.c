5e3c8f79f65748cd13fe29872ee4d2dc34262d98
Kenedy <kenedydacosta@gmail.com>
2023-12-12 16:22:09 -0300

    exercicio soma

---
+++ soma.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int num1, num2;
+    printf("Introduza 02 numeros: ");
+    scanf("%d%d",&num1,&num2);
+    printf("o resultado da soma dos numeros e: %d",num1+num2);
+    return 0;
+}
